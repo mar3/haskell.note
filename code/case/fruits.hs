5e76e1839069e109e86bd0408b3df64cbc5fcb6a
mass10 <irisawa.masaru@gmail.com>
2016-05-22 11:42:20 +0900

    wip

--- code/case/fruits.hs
+++ code/case/fruits.hs
@@ -3,6 +3,10 @@
 
 _what_is_this :: String -> String
 _what_is_this something = do
+
+	-- case もパターンマッチの一つです。
+	-- otherwise が存在しない場合、実行時エラーが発生します。
+	--                 "Non-exhaustive patterns in case"
 	case something of
 		"apple" -> "APPLE"
 		"orange" -> "ORANGE"
@@ -10,7 +14,9 @@ _what_is_this something = do
 
 main :: IO ()
 main = do
+
 	print $ _what_is_this "orange"
 	print $ _what_is_this "apple"
 	print $ _what_is_this "apples"
+
 	return ()
